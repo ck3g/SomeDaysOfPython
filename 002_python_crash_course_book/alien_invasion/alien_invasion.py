4f794167be8a73819034144db37d9dce01481262
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-15 10:51:42 +0200

    Create freet of aliens

--- 002_python_crash_course_book/alien_invasion/alien_invasion.py
+++ 002_python_crash_course_book/alien_invasion/alien_invasion.py
@@ -105,9 +105,22 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Creat the fleet of aliens."""
-        # Make an alien
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens in on alien width.
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            self._create_alien(current_x)
+            current_x += 2 * alien_width
+
+    def _create_alien(self, x_position):
+        """Create an alien and place it in the row"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        self.aliens.add(new_alien)
 
 
 if __name__ == "__main__":
