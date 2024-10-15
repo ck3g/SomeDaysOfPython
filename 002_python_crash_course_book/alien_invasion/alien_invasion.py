f04ef39fd942f52721ca1e7642982d25f5be00d3
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-15 09:36:27 +0200

    Moving the ship left and right

--- 002_python_crash_course_book/alien_invasion/alien_invasion.py
+++ 002_python_crash_course_book/alien_invasion/alien_invasion.py
@@ -6,12 +6,13 @@ from settings import Settings
 from ship import Ship
 
 
+# pylint: disable=no-member
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
-        pygame.init()  # pylint: disable=no-member
+        pygame.init()
         self.clock = pygame.time.Clock()
 
         self.settings = Settings()
@@ -26,14 +27,25 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # pylint: disable=no-member
+            if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
