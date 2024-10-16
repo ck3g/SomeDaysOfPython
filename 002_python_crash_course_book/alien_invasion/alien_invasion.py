bc7dd4c51a155d3fd5f24e01bfdaa4d7c71a244c
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-16 16:13:22 +0200

    Hide mouse cursor when the game starts

--- 002_python_crash_course_book/alien_invasion/alien_invasion.py
+++ 002_python_crash_course_book/alien_invasion/alien_invasion.py
@@ -89,6 +89,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
@@ -230,6 +233,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
 
 if __name__ == "__main__":
