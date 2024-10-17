a885d8f53f5813f856e75068bb2784a6fda59b5e
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-17 11:53:10 +0200

    Display number of ships left

--- 002_python_crash_course_book/alien_invasion/alien_invasion.py
+++ 002_python_crash_course_book/alien_invasion/alien_invasion.py
@@ -86,6 +86,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -241,6 +242,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships left.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
