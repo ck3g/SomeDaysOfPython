d14cfb008611bcef4a7ce947039bbcdfb72f878e
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-20 11:04:16 +0200

    Add detail topic page

--- 002_python_crash_course_book/learning_log/learning_logs/views.py
+++ 002_python_crash_course_book/learning_log/learning_logs/views.py
@@ -12,6 +12,14 @@ def index(request):
 
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by("date_added")
-    context = {"topics": topics}
+    all_topics = Topic.objects.order_by("date_added")  # pylint: disable=no-member
+    context = {"topics": all_topics}
     return render(request, "learning_logs/topics.html", context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries"""
+    one_topic = Topic.objects.get(id=topic_id)  # pylint: disable=no-member
+    entries = one_topic.entry_set.order_by("-date_added")
+    context = {"topic": one_topic, "entries": entries}
+    return render(request, "learning_logs/topic.html", context)
