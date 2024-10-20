9115a371062ec7c92619f1471af0a103fbfa0f56
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-20 09:38:13 +0200

    Add Entry model

--- 002_python_crash_course_book/learning_log/learning_logs/models.py
+++ 002_python_crash_course_book/learning_log/learning_logs/models.py
@@ -11,4 +11,19 @@ class Topic(models.Model):
 
     def __str__(self):
         """Return a string representation of the model."""
-        return self.text
+        return str(self.text)
+
+
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Return a simple string representing the entry."""
+        return f"{str(self.text)[:50]}..."
