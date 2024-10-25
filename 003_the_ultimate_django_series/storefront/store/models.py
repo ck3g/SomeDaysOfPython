bcc81e30a3f6441e08a0067c85c3fde58f5f9279
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-25 16:00:35 +0200

    Validation example

--- 003_the_ultimate_django_series/storefront/store/models.py
+++ 003_the_ultimate_django_series/storefront/store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -30,13 +31,15 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(1)]
+    )
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(
-        Promotion
+        Promotion, blank=True
     )  # Optionally can set related_name="products"
 
     def __str__(self) -> str:
