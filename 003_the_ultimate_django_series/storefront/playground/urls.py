54ce4cc5fc1e7e54622c5aad817ad8047968c956
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-25 13:24:06 +0200

    Add TagItemManager class

--- 003_the_ultimate_django_series/storefront/playground/urls.py
+++ 003_the_ultimate_django_series/storefront/playground/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("hello/", views.say_hello),
+    path("products/", views.products),
 ]
--- 003_the_ultimate_django_series/storefront/playground/views.py
+++ 003_the_ultimate_django_series/storefront/playground/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 
 def say_hello(request):
-    x = calculate()
     return render(request, "hello.html", {"name": "John"})
+
+
+def products(request):
+    query_set = Product.objects.filter(  # pylint: disable=no-member
+        title__icontains="coffee"
+    )
+
+    return render(request, "hello.html", {"name": "John", "products": list(query_set)})
