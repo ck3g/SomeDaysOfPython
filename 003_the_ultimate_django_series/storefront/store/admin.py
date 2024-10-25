351fa1d72393767c2e5af06b144ba2ea4db95520
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-25 15:11:30 +0200

    Add orders count to customer

--- 003_the_ultimate_django_series/storefront/store/admin.py
+++ 003_the_ultimate_django_series/storefront/store/admin.py
@@ -45,10 +45,21 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "membership"]
+    list_display = ["first_name", "last_name", "membership", "orders_count"]
     list_editable = ["membership"]
     ordering = ["first_name", "last_name"]
     list_per_page = 10
+
+    def orders_count(self, customer):
+        url = (
+            reverse("admin:store_order_changelist")
+            + "?"
+            + urlencode({"customer__id": str(customer.id)})
+        )
+        return format_html("<a href='{}'>{}</a>", url, customer.orders_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(orders_count=Count("order"))
 
 
 @admin.register(models.Order)
