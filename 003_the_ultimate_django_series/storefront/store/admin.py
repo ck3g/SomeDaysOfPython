6379a8e5f4a777275b9b058a94ad8e6ad3218d56
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-25 15:29:53 +0200

    Define custom actions

--- 003_the_ultimate_django_series/storefront/store/admin.py
+++ 003_the_ultimate_django_series/storefront/store/admin.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models.query import QuerySet
 from django.db.models import Count
 from django.http import HttpRequest
@@ -23,6 +23,7 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    actions = ["clear_inventory"]
     list_display = ["title", "collection_title", "unit_price", "inventory_status"]
     list_editable = ["unit_price"]
     list_filter = ["collection", "last_update", InventoryFilter]
@@ -38,6 +39,15 @@ class ProductAdmin(admin.ModelAdmin):
 
     def collection_title(self, product):
         return product.collection.title
+
+    @admin.action(description="Clear inventory")
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)  # real update
+        self.message_user(
+            request,
+            f"{updated_count} products were successfuly updated",
+            messages.SUCCESS,
+        )
 
 
 @admin.register(models.Collection)
