914283ee97d12a501b4d7161e661c70f39614a3b
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-25 14:59:21 +0200

    Add products count to the collection

--- 003_the_ultimate_django_series/storefront/store/admin.py
+++ 003_the_ultimate_django_series/storefront/store/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.db.models import Count
+from django.http import HttpRequest
 
 from . import models
 
@@ -21,7 +24,16 @@ class ProductAdmin(admin.ModelAdmin):
         return product.collection.title
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title", "products_count"]
+
+    @admin.display(ordering="products_count")
+    def products_count(self, collection):
+        return collection.products_count
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(products_count=Count("product"))
 
 
 @admin.register(models.Customer)
