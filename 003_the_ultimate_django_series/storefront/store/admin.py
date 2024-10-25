99eeed61e7ba31196f46011b5e6a75fdab2294ba
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-25 15:06:41 +0200

    Add customizeable links

--- 003_the_ultimate_django_series/storefront/store/admin.py
+++ 003_the_ultimate_django_series/storefront/store/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.db.models.query import QuerySet
 from django.db.models import Count
 from django.http import HttpRequest
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 
 from . import models
 
@@ -30,7 +32,12 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering="products_count")
     def products_count(self, collection):
-        return collection.products_count
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__id": str(collection.id)})
+        )
+        return format_html("<a href='{}'>{}</a>", url, collection.products_count)
 
     def get_queryset(self, request: HttpRequest) -> QuerySet:
         return super().get_queryset(request).annotate(products_count=Count("product"))
