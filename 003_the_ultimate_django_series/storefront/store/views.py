608d646456d4fa8266011ef1c908dbe91043906b
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-26 11:12:42 +0200

    Create a products POST request

--- 003_the_ultimate_django_series/storefront/store/views.py
+++ 003_the_ultimate_django_series/storefront/store/views.py
@@ -6,12 +6,19 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def product_list(request):
-    # pylint: disable=no-member
-    queryset = Product.objects.select_related("collection").all()
-    serializer = ProductSerializer(queryset, many=True, context={"request": request})
-    return Response(serializer.data)
+    if request.method == "GET":
+        # pylint: disable=no-member
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        # serializer.validated_data
+        return Response("ok")
 
 
 @api_view()
