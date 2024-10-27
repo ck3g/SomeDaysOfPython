50eb74e5a3bd9ed84ee97d794864dbc425d8a74b
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-27 08:37:46 +0100

    Use generic view for product list

--- 003_the_ultimate_django_series/storefront/store/views.py
+++ 003_the_ultimate_django_series/storefront/store/views.py
@@ -4,24 +4,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+
+from rest_framework.generics import ListCreateAPIView
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-class ProductList(APIView):
-    def get(self, request):
-        # pylint: disable=no-member
-        queryset = Product.objects.select_related("collection").all()
-        serializer = ProductSerializer(
-            queryset, many=True, context={"request": request}
-        )
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+    # pylint: disable=no-member
+    queryset = Product.objects.select_related("collection").all()
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # Alternatively can use methods
+    # def get_queryset(self):
+    #     # pylint: disable=no-member
+    #     return Product.objects.select_related("collection").all()
+
+    def get_serializer_class(self):
+        return ProductSerializer
+
+    def get_serializer_context(self):
+        return {"request": self.request}
 
 
 class ProductDetail(APIView):
