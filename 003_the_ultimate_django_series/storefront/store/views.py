5219cc106eaa8a6464cd8a772c1bedeed3f26f6b
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-28 12:36:41 +0100

    Add delete cart endpoint

--- 003_the_ultimate_django_series/storefront/store/views.py
+++ 003_the_ultimate_django_series/storefront/store/views.py
@@ -3,7 +3,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    RetrieveModelMixin,
+    DestroyModelMixin,
+)
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
@@ -79,7 +83,9 @@ class ReviewViewSet(ModelViewSet):
         return {"product_id": self.kwargs["product_id"]}
 
 
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class CartViewSet(
+    CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet
+):
     serializer_class = CartSerializer
     lookup_field = "id"
     queryset = Cart.objects.prefetch_related("items__product").all()
