d2eeb3932317b065c8da69e5bb3fe8724fb42e95
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-29 09:26:39 +0100

    Extend a user model: make email unique

--- 003_the_ultimate_django_series/storefront/core/admin.py
+++ 003_the_ultimate_django_series/storefront/core/admin.py
@@ -1,10 +1,33 @@
 from django.contrib.contenttypes.admin import GenericTabularInline
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from store.admin import ProductAdmin
 from store.models import Product
 from tags.models import TagItem
+from .models import User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
 
 
 class TagInline(GenericTabularInline):
--- 003_the_ultimate_django_series/storefront/core/models.py
+++ 003_the_ultimate_django_series/storefront/core/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+# pylint: disable=no-member
+
+
+class User(AbstractUser):
+    # IMPORTANT!!!
+    # If we create this class in the middle of the development, we're going to get the following error.
+    # Migration admin.0001_initial is applied before its dependency core.0001_initial on database 'default'.
+    #
+    # To solve the error, we need to recreate the whole database and run migrations from scratch.
+    # So, the better approach, when you're starting a new project, create this model right away with the blank body
+    #
+    # pass
+    email = models.EmailField(unique=True)
