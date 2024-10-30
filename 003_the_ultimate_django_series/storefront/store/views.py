b127d093011cf07924b0e04b10d92be86ee319ed
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-10-30 15:21:52 +0100

    Create /customers/me endpoint

--- 003_the_ultimate_django_series/storefront/store/views.py
+++ 003_the_ultimate_django_series/storefront/store/views.py
@@ -11,6 +11,7 @@ from rest_framework.mixins import (
 )
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.decorators import action
 
 from .models import Product, Collection, OrderItem, Review, Cart, CartItem, Customer
 from .serializers import (
@@ -121,3 +122,17 @@ class CustomerViewSet(
 ):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    # detail=False produces /customers/me
+    # detail=True produces /customers/:id/me
+    @action(detail=False, methods=["GET", "PUT"])
+    def me(self, request):
+        customer, _created = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == "GET":
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == "PUT":
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
