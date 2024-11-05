da917fff69d60fcecdd03762df463ac6cd4d6765
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-05 11:16:55 +0100

    Adding a test

---
+++ 003_the_ultimate_django_series/storefront/store/tests/test_collections.py
@@ -0,0 +1,19 @@
+from rest_framework import status
+from rest_framework.test import APIClient
+import pytest
+
+
+@pytest.mark.django_db
+class TestCreateColelction:
+    # @pytest.mark.skip # Skipping tests
+    def test_if_user_is_anonymous_returns_401(self):
+        # AAA (Arrange, Act, Assert)
+
+        # Arrange
+
+        # Act
+        client = APIClient()
+        response = client.post("/store/collections/", {"title": "Test Collection"})
+
+        # Assert
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
