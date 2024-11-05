26f52a99b84e70480cec376f366104b3018e6448
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-05 11:44:27 +0100

    Add auth_user fixture

--- 003_the_ultimate_django_series/storefront/store/tests/test_collections.py
+++ 003_the_ultimate_django_series/storefront/store/tests/test_collections.py
@@ -4,6 +4,14 @@ import pytest
 
 
 @pytest.fixture
+def auth_user(api_client):
+    def do_auth_user(user):
+        api_client.force_authenticate(user=user)
+
+    return do_auth_user
+
+
+@pytest.fixture
 def create_collection(api_client):
     def do_create_collection(collection):
         return api_client.post("/store/collections/", collection)
@@ -25,21 +33,21 @@ class TestCreateCollection:
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self, api_client, create_collection):
-        api_client.force_authenticate(user={})
+    def test_if_user_is_not_admin_returns_403(self, auth_user, create_collection):
+        auth_user({})
         response = create_collection({"title": "Test Collection"})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_returns_400(self, api_client, create_collection):
-        api_client.force_authenticate(user=User(is_staff=True))
+    def test_if_data_is_invalid_returns_400(self, auth_user, create_collection):
+        auth_user(User(is_staff=True))
         response = create_collection({"title": ""})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data["title"] is not None
 
-    def test_if_data_is_valid_returns_201(self, api_client, create_collection):
-        api_client.force_authenticate(user=User(is_staff=True))
+    def test_if_data_is_valid_returns_201(self, auth_user, create_collection):
+        auth_user(User(is_staff=True))
         response = create_collection({"title": "Test Collection"})
 
         assert response.status_code == status.HTTP_201_CREATED
