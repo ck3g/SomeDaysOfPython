6595ca0370d2e97300608c80501b14a1701a00e5
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-10 10:57:10 +0100

    Connect to the database and read todos data

--- 005_fastapi_the_complete_course_2024/todos/main.py
+++ 005_fastapi_the_complete_course_2024/todos/main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
+# only runs then the database file is not exists
 models.Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+DBDependency = Annotated[Session, Depends(get_db)]
+
+
 @app.get("/")
-async def welcome():
-    return {"app": "TODOs"}
+async def read_all(db: DBDependency):
+    return db.query(Todos).all()
