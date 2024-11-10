05f7703d1c42f891c24d4080d4d12e2f1ee58524
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-10 09:23:47 +0100

    Add published date to books

--- 005_fastapi_the_complete_course_2024/books/books2.py
+++ 005_fastapi_the_complete_course_2024/books/books2.py
@@ -13,13 +13,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -28,6 +30,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=0, max_length=100)
     rating: int = Field(gt=-1, lt=6)
+    published_date: int = Field(gt=1900, lt=2030)
 
     model_config = {
         "json_schema_extra": {
@@ -36,25 +39,41 @@ class BookRequest(BaseModel):
                 "author": "John Doe",
                 "description": "Description of the book",
                 "rating": 5,
+                "published_date": 2024,
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, "Fluent Python", "Luciano Ramalho", "Fluent description", 5),
-    Book(2, "Python testing with pytest", "Brian Okken", "Testing description", 4),
+    Book(1, "Fluent Python", "Luciano Ramalho", "Fluent description", 5, 2023),
+    Book(
+        2, "Python testing with pytest", "Brian Okken", "Testing description", 4, 2022
+    ),
     Book(
         3,
         "Designing Data-Intensive Applications",
         "Martin Kleppmann",
         "Designing description",
         3,
+        2016,
     ),
-    Book(4, "Distributed systems", "Van Steen Tanenbaum", "Distributed description", 5),
-    Book(5, "Python Crash Course", "Eric Matthes", "Crash description", 4),
     Book(
-        6, "The shortest history of Germany", "James Hawes", "Shortest description", 3
+        4,
+        "Distributed systems",
+        "Van Steen Tanenbaum",
+        "Distributed description",
+        5,
+        2015,
+    ),
+    Book(5, "Python Crash Course", "Eric Matthes", "Crash description", 4, 2022),
+    Book(
+        6,
+        "The shortest history of Germany",
+        "James Hawes",
+        "Shortest description",
+        3,
+        2010,
     ),
 ]
 
