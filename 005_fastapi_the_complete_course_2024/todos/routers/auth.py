1785abf2b4c7551ae1b32425cc563421daa447a3
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-11 11:15:29 +0100

    Store a new user to the database

--- 005_fastapi_the_complete_course_2024/todos/routers/auth.py
+++ 005_fastapi_the_complete_course_2024/todos/routers/auth.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from starlette import status
 from passlib.context import CryptContext
 
+from database import SessionLocal
 from models import Users
 
 router = APIRouter()
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+DBDependency = Annotated[Session, Depends(get_db)]
 
 
 class CreateUserRequest(BaseModel):
@@ -21,7 +35,7 @@ class CreateUserRequest(BaseModel):
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
-async def create_user(create_user_request: CreateUserRequest):
+async def create_user(db: DBDependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -31,5 +45,8 @@ async def create_user(create_user_request: CreateUserRequest):
         hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True,
     )
+
+    db.add(create_user_model)
+    db.commit()
 
     return create_user_model
