2301c4c9b8f8a1b11daa9a7d65081b4a0bb5243b
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-11 12:10:15 +0100

    Decode JWT token

--- 005_fastapi_the_complete_course_2024/todos/routers/auth.py
+++ 005_fastapi_the_complete_course_2024/todos/routers/auth.py
@@ -1,12 +1,12 @@
 from datetime import timedelta, datetime, timezone
 from typing import Annotated
-from fastapi import APIRouter, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from starlette import status
 from passlib.context import CryptContext
-from jose import jwt
+from jose import jwt, JWTError
 
 from database import SessionLocal
 from models import Users
@@ -19,6 +19,7 @@ ALGORITHM = "HS256"
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def get_db():
@@ -48,6 +49,24 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+            )
+        return {"username": username, "id": user_id}
+    except JWTError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        ) from e
 
 
 class CreateUserRequest(BaseModel):
@@ -89,7 +108,10 @@ async def create_token(
 
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return "Failed auth"
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+        )
 
     token = create_access_token(user.username, user.id, timedelta(minutes=20))
 
