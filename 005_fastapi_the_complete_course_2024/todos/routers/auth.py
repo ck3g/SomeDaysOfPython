588e77f23a5bda82626329aa802534064eaa9923
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-11 11:10:52 +0100

    Hash a user's password

--- 005_fastapi_the_complete_course_2024/todos/routers/auth.py
+++ 005_fastapi_the_complete_course_2024/todos/routers/auth.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from starlette import status
+from passlib.context import CryptContext
 
 from models import Users
 
 router = APIRouter()
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class CreateUserRequest(BaseModel):
@@ -24,7 +28,7 @@ async def create_user(create_user_request: CreateUserRequest):
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True,
     )
 
