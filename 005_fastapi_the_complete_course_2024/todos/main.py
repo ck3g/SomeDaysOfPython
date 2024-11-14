2514e79bcbd3ab9dc2fe407541f7568c01cde6d6
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-14 13:21:01 +0100

    Create a healthy endpoint and test for it

--- 005_fastapi_the_complete_course_2024/todos/main.py
+++ 005_fastapi_the_complete_course_2024/todos/main.py
@@ -14,3 +14,8 @@ app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(admin.router)
 app.include_router(todos.router)
+
+
+@app.get("/healthy")
+def health_check():
+    return {"status": "healthy"}
