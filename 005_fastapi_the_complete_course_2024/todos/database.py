65ccfdce975ad255b08ecc788f8f54958df3fc17
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-14 16:05:22 +0100

    Configure test db and test todos endpoint with empty data

--- 005_fastapi_the_complete_course_2024/todos/database.py
+++ 005_fastapi_the_complete_course_2024/todos/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@127.0.0.1:3306/todoapp"
