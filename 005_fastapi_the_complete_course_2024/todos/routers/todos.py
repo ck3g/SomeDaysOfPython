ed725f4d9cb78c8004b45be93c46a80ac91caad5
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-16 09:49:18 +0100

    Add a todo list page

--- 005_fastapi_the_complete_course_2024/todos/routers/todos.py
+++ 005_fastapi_the_complete_course_2024/todos/routers/todos.py
@@ -1,7 +1,9 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path, Request
+from fastapi.templating import Jinja2Templates
 from starlette import status
+from starlette.responses import RedirectResponse
 from pydantic import BaseModel, Field
 
 from models import Todos
@@ -9,6 +11,8 @@ from database import SessionLocal
 from .auth import get_current_user
 
 router = APIRouter(prefix="/todos", tags=["todos"])
+
+templates = Jinja2Templates(directory="templates")
 
 
 def get_db():
@@ -28,6 +32,37 @@ class TodoRequest(BaseModel):
     description: str = Field(min_length=3, max_length=100)
     priority: int = Field(gt=0, lt=6)
     complete: bool
+
+
+def redirect_to_login():
+    redirect_response = RedirectResponse(
+        url="/auth/login-page", status_code=status.HTTP_302_FOUND
+    )
+    redirect_response.delete_cookie(key="access_token")
+    return redirect_response
+
+
+### Pages ###
+
+
+@router.get("/todo-page")
+async def todo_page(request: Request, db: DBDependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+        if user is None:
+            return redirect_to_login()
+
+        todos = db.query(Todos).filter(Todos.owner_id == user.get("id")).all()
+
+        return templates.TemplateResponse(
+            "todo.html", {"request": request, "todos": todos, "user": user}
+        )
+
+    except:
+        return redirect_to_login()
+
+
+### Endpoints ###
 
 
 @router.get("/")
