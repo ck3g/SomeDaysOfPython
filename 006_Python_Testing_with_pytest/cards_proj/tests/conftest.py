8970324129adc27b0284e61f9ef35371fd7f920f
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-18 11:54:15 +0100

    Add a clean up database fixture

--- 006_Python_Testing_with_pytest/cards_proj/tests/conftest.py
+++ 006_Python_Testing_with_pytest/cards_proj/tests/conftest.py
@@ -5,11 +5,18 @@ import cards
 
 
 @pytest.fixture(scope="session")
-def cards_db():
+def db():
     """CardsDB object connected to a temporary database."""
     with TemporaryDirectory() as db_dir:
         db_path = Path(db_dir)
-        db = cards.CardsDB(db_path)
+        db_ = cards.CardsDB(db_path)
 
-        yield db
-        db.close()
+        yield db_
+        db_.close()
+
+
+@pytest.fixture(scope="function")
+def cards_db(db):
+    """CardsDB object that's empty"""
+    db.delete_all()
+    return db
