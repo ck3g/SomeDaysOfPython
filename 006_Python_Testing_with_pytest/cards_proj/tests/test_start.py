354ba06aa5b7242c91ce15c4bdd07446273ef7fc
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-20 09:52:42 +0100

    Marking tests as: smoke, exception

--- 006_Python_Testing_with_pytest/cards_proj/tests/test_start.py
+++ 006_Python_Testing_with_pytest/cards_proj/tests/test_start.py
@@ -1,5 +1,5 @@
 import pytest
-from cards import Card
+from cards import Card, InvalidCardId
 
 
 @pytest.mark.parametrize("start_state", ["done", "in prog", "todo"])
@@ -9,3 +9,26 @@ def test_start(cards_db, start_state):
     cards_db.start(index)
     card = cards_db.get_card(index)
     assert card.state == "in prog"
+
+
+# run using `pytest -v -m smoke`
+@pytest.mark.smoke
+def test_start_smoke(cards_db):
+    """
+    Start changes state from "todo" to "in prog"
+    """
+    i = cards_db.add_card(Card("foo", state="todo"))
+    cards_db.start(i)
+    c = cards_db.get_card(i)
+    assert c.state == "in prog"
+
+
+# run using `pytest -v -m exception`
+@pytest.mark.exception
+def test_start_non_existent(cards_db):
+    """
+    Shouldn't be able to start a non-existent card.
+    """
+    any_number = 123  # any number will be invalid, db is empty
+    with pytest.raises(InvalidCardId):
+        cards_db.start(any_number)
