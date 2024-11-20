7a5d21c17c4d5be553fe2459bd489ac4e7498327
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-20 09:01:49 +0100

    Parametrize tests

---
+++ 006_Python_Testing_with_pytest/cards_proj/tests/test_finish.py
@@ -0,0 +1,60 @@
+import pytest
+from cards import Card
+
+
+###
+# NO PARAMETRIZE EXAMPLE
+###
+
+
+def test_finish_from_in_prog(cards_db):
+    index = cards_db.add_card(Card("second edition", state="in prog"))
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
+
+
+def test_finish_from_done(cards_db):
+    index = cards_db.add_card(Card("second edition", state="done"))
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
+
+
+def test_finish_from_todo(cards_db):
+    index = cards_db.add_card(Card("second edition", state="done"))
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
+
+
+###
+# PARAMETRIZE EXAMPLE
+###
+
+
+@pytest.mark.parametrize(
+    "start_summary, start_state",
+    [
+        ("write a book", "done"),
+        ("second editition", "in prog"),
+        ("create a course", "todo"),
+    ],
+)
+def test_finish(cards_db, start_summary, start_state):
+    initial_card = Card(summary=start_summary, state=start_state)
+    index = cards_db.add_card(initial_card)
+
+    cards_db.finish(index)
+
+    card = cards_db.get_card(index)
+    assert card.state == "done"
+
+
+@pytest.mark.parametrize("start_state", ["done", "in prog", "todo"])
+def test_finish_simple(cards_db, start_state):
+    c = Card("write a book", state=start_state)
+    index = cards_db.add_card(c)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
