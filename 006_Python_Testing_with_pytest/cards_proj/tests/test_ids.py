30f1cae5c9f227e26f3276e1427a39fafe398a20
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-27 11:43:39 +0100

    Creating custom identifiers

--- 006_Python_Testing_with_pytest/cards_proj/tests/test_ids.py
+++ 006_Python_Testing_with_pytest/cards_proj/tests/test_ids.py
@@ -15,3 +15,18 @@ def test_card(cards_db, starting_card):
     cards_db.finish(index)
     card = cards_db.get_card(index)
     assert card.state == "done"
+
+
+card_list = [
+    Card("foo", state="todo"),
+    Card("foo", state="in prog"),
+    Card("foo", state="done"),
+]
+
+
+@pytest.mark.parametrize("starting_card", card_list, ids=str)
+def test_id_str(cards_db, starting_card):
+    index = cards_db.add_card(starting_card)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
