6e8068623badace40591fb44f6ea776bed32fa9f
Vitali Tatarintev <vtatarintev@gitlab.com>
2024-11-27 11:45:57 +0100

    Writing custom ID functions

--- 006_Python_Testing_with_pytest/cards_proj/tests/test_ids.py
+++ 006_Python_Testing_with_pytest/cards_proj/tests/test_ids.py
@@ -30,3 +30,23 @@ def test_id_str(cards_db, starting_card):
     cards_db.finish(index)
     card = cards_db.get_card(index)
     assert card.state == "done"
+
+
+def card_state(card):
+    return card.state
+
+
+@pytest.mark.parametrize("starting_card", card_list, ids=card_state)
+def test_id_func(cards_db, starting_card):
+    index = cards_db.add_card(starting_card)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
+
+
+@pytest.mark.parametrize("starting_card", card_list, ids=lambda c: c.state)
+def test_id_lambda(cards_db, starting_card):
+    index = cards_db.add_card(starting_card)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
